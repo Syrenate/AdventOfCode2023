e9c34519ccb064518cbdb12c264cedf2e4b044c5
Luke J <luke.jennings1@outlook.com>
2023-12-01 19:42:41 +0000

    Update Day1.hs
    unnecessary code

--- Day1.hs
+++ Day1.hs
@@ -7,14 +7,6 @@ getInput = do readFile "Day1Input.txt"
 digitNames = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 digitVals  = ['1','2','3','4','5','6','7','8','9']
 
-mapNameToDigit :: String -> Char
-mapNameToDigit xs = head [i | (nam,i) <- zip digitNames digitVals, nam == xs]
-
-toDigitPart1 :: String -> String
-toDigitPart1 [] = []
-toDigitPart1 s | or [[n] `isPrefixOf` s | n <- digitVals] = head s : toDigitPart1 (tail s)
-			   | otherwise = toDigitPart1 $ tail s
-
 toDigit :: Int -> String -> String
 toDigit _ [] = []
 toDigit n s | or [[n] `isPrefixOf` s | n <- digitVals] = head s : toDigit n (tail s)
@@ -34,19 +26,9 @@ isPrefixOf xs' xs | length xs < length xs'      = False
 				  | take (length xs') xs == xs' = True
 				  | otherwise                   = False
 
-
-calibrationValue :: [Int] -> Int
-calibrationValue xs = 10 * head xs + last xs
-
-part1 :: String -> Int
-part1 = getAnswer 1
-
-part2 :: String -> Int
-part2 = getAnswer 2 
-
 getAnswer :: Int -> String -> Int
-getAnswer n xs = sum $ map (calibrationValue . map (\x -> ord x - 48) . toDigit n) (getResult $ parse sepLines xs)
+getAnswer n xs = sum $ map ((\xs' -> (10 * head xs' + last xs')) . map (\x -> ord x - 48) . toDigit n) (getResult $ parse sepLines xs)
 
 main = do inp <- getInput
-          print ("Part 1: " ++ show (part1 inp))
-          print ("Part 2: " ++ show (part2 inp))
+          print ("Part 1: " ++ show (getAnswer 1 inp))
+          print ("Part 2: " ++ show (getAnswer 2 inp))
